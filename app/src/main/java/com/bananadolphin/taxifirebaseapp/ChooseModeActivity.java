4b4c36ee9a8261f62703f4117c588e5418435c2d
Max <maxusan@gmail.com>
2021-03-21 19:59:28 +0200

    baka

--- app/src/main/java/com/bananadolphin/taxifirebaseapp/ChooseModeActivity.java
+++ app/src/main/java/com/bananadolphin/taxifirebaseapp/ChooseModeActivity.java
@@ -2,6 +2,7 @@ package com.bananadolphin.taxifirebaseapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class ChooseModeActivity extends AppCompatActivity {
@@ -9,6 +10,27 @@ public class ChooseModeActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sign_in);
+        setContentView(R.layout.activity_splash_screen);
+
+        Thread thread = new Thread(){
+            @Override
+            public void run() {
+                super.run();
+                try{
+                    sleep(5000);
+                }catch (Exception e){
+                    e.printStackTrace();
+                }finally {
+                    startActivity(new Intent(ChooseModeActivity.this, ChooseModeActivity.class));
+                }
+            }
+        };
+        thread.start();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        finish();
     }
 }
