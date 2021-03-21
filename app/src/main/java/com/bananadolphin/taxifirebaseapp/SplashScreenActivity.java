cf66e3156aef0009a0290694162e48a2a6b6d233
Max <maxusan@gmail.com>
2021-03-21 19:55:49 +0200

    Splash screen activity logic

--- app/src/main/java/com/bananadolphin/taxifirebaseapp/SplashScreenActivity.java
+++ app/src/main/java/com/bananadolphin/taxifirebaseapp/SplashScreenActivity.java
@@ -2,6 +2,7 @@ package com.bananadolphin.taxifirebaseapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class SplashScreenActivity extends AppCompatActivity {
@@ -10,5 +11,26 @@ public class SplashScreenActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+        Thread thread = new Thread(){
+            @Override
+            public void run() {
+                super.run();
+                try{
+                    sleep(5000);
+                }catch (Exception e){
+                    e.printStackTrace();
+                }finally {
+                    startActivity(new Intent(SplashScreenActivity.this, SignInActivity.class));
+                }
+            }
+        };
+        thread.start();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        finish();
     }
 }
