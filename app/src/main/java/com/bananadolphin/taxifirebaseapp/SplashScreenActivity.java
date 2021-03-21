15c7dc5e6fbe41d883cfa5fe46d590d20c1d55f5
Max <maxusan@gmail.com>
2021-03-21 19:57:47 +0200

    baka

--- app/src/main/java/com/bananadolphin/taxifirebaseapp/SplashScreenActivity.java
+++ app/src/main/java/com/bananadolphin/taxifirebaseapp/SplashScreenActivity.java
@@ -10,7 +10,7 @@ public class SplashScreenActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash_screen);
+        setContentView(R.layout.activity_choose_mode);
 
         Thread thread = new Thread(){
             @Override
